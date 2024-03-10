2f52bdfdff70ca639b929ec59a4dff0dfefd4071
Abyss <finalparanoia@outlook.com>
2024-03-11 03:18:37 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const text = ref('')
+
+const update = () => {
+  fetch('http://127.0.0.1:8000/text/')
+    .then((data) => data.json())
+    .then((data) => {
+      text.value = data
+    })
+}
+
+
+setInterval(() => {
+  update()
+}, 1000);
+
+</script>
+
+<template>
+  {{ text }}
+</template>
